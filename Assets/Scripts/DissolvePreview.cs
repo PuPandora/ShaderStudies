8e26e9a31a35d16f30b2c346e01c5f55105437f9
Pandora <103708353+PuPandora@users.noreply.github.com>
2026-09-10 18:55:58 +0900

    feat: Dissolve 셰이더 개선
    - 없어지는, 생기는 일정 부분에 HDR 컬러를 넣어 빛나는(Emission) 효과를 추가함
    - 여러 개의 오브젝트일 때 다른 진행도, 색상을 가질 수 있도록 함

--- Assets/Scripts/DissolvePreview.cs
+++ Assets/Scripts/DissolvePreview.cs
@@ -1,32 +1,68 @@
-using System;
 using UnityEngine;
 
 public class DissolvePreview : MonoBehaviour
 {
+    [Header("속성")]
     [SerializeField] private Renderer _targetRenderer;
-    [SerializeField] private string _cutoffPropertyName = "_Cutoff";
     [SerializeField] private float _speed = 0.5f;
     [SerializeField] private float _min = 0f;
     [SerializeField] private float _max = 1f;
-
+    [SerializeField] private bool _startRandomCutoff = true;
+    [ColorUsage(true, true)]
+    [SerializeField] private Color _dissolveColor = Color.white;
+    
+    [Header("Property Hash ID")]
+    [SerializeField] private string _cutoffPropertyName = "_Cutoff";
+    [SerializeField] private string _dissolveColorPropertyName = "_DissolveColor";
     private int _cutoffId;
+    private int _dissolveColorId;
+    
     private MaterialPropertyBlock _block;
+    private float _dissolveTime;
 
     private void Awake()
     {
         if (_targetRenderer == null)
             _targetRenderer = GetComponent<Renderer>();
 
+        if (_startRandomCutoff)
+        {
+            _dissolveTime = Random.Range(0f, 10f);
+        }
         _cutoffId = Shader.PropertyToID(_cutoffPropertyName);
+        _dissolveColorId = Shader.PropertyToID(_dissolveColorPropertyName);
+        
         _block = new MaterialPropertyBlock();
+        
+        // 검사
+        if (!_targetRenderer.sharedMaterial.HasProperty(_cutoffId))
+        {
+            Debug.LogError($"[Dissolve] {_cutoffPropertyName} 프로퍼티가 존재하지 않습니다.");
+        }
+
+        if (!_targetRenderer.sharedMaterial.HasProperty(_dissolveColorId))
+        {
+            Debug.LogError($"[Dissolve] {_dissolveColorPropertyName} 프로퍼티가 존재하지 않습니다.");
+        }
+    }
+
+    private void OnValidate()
+    {
+        _cutoffId = Shader.PropertyToID(_cutoffPropertyName);
+        _dissolveColorId = Shader.PropertyToID(_dissolveColorPropertyName);
     }
 
     private void Update()
     {
-        float value = Mathf.Lerp(_min, _max, Mathf.PingPong(Time.time * _speed, 1f));
-
-        _targetRenderer.GetPropertyBlock(_block);
+        // Cutoff
+        float value = Mathf.Lerp(_min, _max, Mathf.PingPong(_dissolveTime * _speed, 1f));
+        _dissolveTime += Time.deltaTime;
+        
         _block.SetFloat(_cutoffId, value);
+        
+        // Color
+        _block.SetColor(_dissolveColorId, _dissolveColor);
+        
         _targetRenderer.SetPropertyBlock(_block);
     }
 }
